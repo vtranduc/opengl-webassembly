645a570df2cbf0b82dbb5ec11226a67ec9400a65
Viet Nhien Tran Duc <vtranduc@adobe.com>
2023-10-31 00:28:32 -0600

    corrected initial front direction

--- engine/headers/math/matrix/view.h
+++ engine/headers/math/matrix/view.h
@@ -20,7 +20,7 @@ private:
     Vector3
         position{ 0.0f },
         target{ 0.0f, 0.0f, -1.0f },
-        front{ 0.0f, 0.0f, 1.0f },
+        front{ 0.0f, 0.0f, -1.0f },
         right{ 1.0f, 0.0f, 0.0f },
         up{ 0.0f, 1.0f, 0.0f },
         tmp, tmp_;
