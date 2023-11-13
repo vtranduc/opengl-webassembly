40cc8ec6574a26fd6f0af4a01ef016d3cd925bdf
vtranduc <vtranduc@adobe.com>
2023-11-13 10:52:35 -0700

    shorten gotPosition and getScale logics in world matrix

--- engine/src/math/matrix/world.cpp
+++ engine/src/math/matrix/world.cpp
@@ -15,8 +15,8 @@ void World::scaleInPlace(const float* xyz) { scaleInPlace(xyz[0], xyz[1], xyz[2]
 
 void World::scaleInPlace(float x, float y, float z) { scale.multiply(x, y, z); setDirty(); };
 
-Vector3& World::getPosition(Vector3* out) const { out->copy(position); return *out; };
+Vector3& World::getPosition(Vector3* out) const { return out->copy(position); };
 
-Vector3& World::getScale(Vector3* out) const { out->copy(scale); return *out; };
+Vector3& World::getScale(Vector3* out) const { return out->copy(scale); };
 
 float World::RotationMatrix::operator()(int i, int j) const { return elements[i][j]; };
